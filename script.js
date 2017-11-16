52b93d0022e33f3286f3221bec94d6fa85c0f0c4
Alex <studio@oioioi.io>
2017-11-16 02:21:34 +0000

    Add new file

---
+++ script.js
@@ -0,0 +1,13 @@
+var narrativeArray = [
+  [['fiuhfiuhfiuhf'], ['nature'], 'day', true, 1000],
+  [['Do not give in to sleep'], ['laundry'], 'day', true, 3000],
+  [['And prisoner of time. '], ['laundry'], 'day', true, 1000],
+  [[''], ['laundry'], 'day', true, 2000],
+  [[''], ['laundry'], 'day', true, 3000],
+  [[''], ['laundry'], 'day', true, 3000],
+  [['iduhdiuh duhdiuhdiuhd'], ['architecture'], 'day', true, 3000],
+  [[''], ['laundry'], 'day', true, 400],
+  [[''], ['pool'], 'day', true, 400],
+  [[''], ['interesting'], 'day', true, 300],
+  [[''], ['street'], 'day', true, 300],
+];
