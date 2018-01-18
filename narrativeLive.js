d992535006372480b5ad1fbad3ae0e6192a074e6
hollygvnalex <33721304+hollygvnalex@users.noreply.github.com>
2018-01-18 18:37:18 +0100

    Create narrativeLive.js

---
+++ narrativeLive.js
@@ -0,0 +1,13 @@
+// ["Beach", "Laundry", "Nature", "Animal", "Interesting", "Hq", "Guess", "Restaurant", "Pool", "Road", "River",
+// "Mountain", "Office", "Bridge", "Architecture", "Street", "City", "Square", "Sport", "Port",
+// "Entertainment", "Bar", "Traffic", "Coffeehouse", "Service", "Mall", "Server", "Construction",
+// "Parking", "House", "Shop", "Farm", "Airliner", "Religion"]
+
+var narrativeArray = [
+  [1000, ['a footnote upon which all other thoughts were had'], 3000, ['Mountain'], 'day', true],
+  [1000, ['there were quite many'], 3000, ['Nature'], 'day', true],
+  [1000, ['think of my voice as it sung a sweet exhale'], 1000, ['Beach'], 'day', true],
+  [1000, ['do you remember it as it was?'], 3000, ['Beach'], 'night', true],
+  [200, ['a JSON fog'], 5000, ['Bar'], 'day', true],
+  [0, ['bug eyed, drippy data'], 1000, ['Bar'], 'day', true],
+];
